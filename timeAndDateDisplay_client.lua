618cbc829af537d12745d073e0bcbadfe5d649bb
TheRealKymac <31000561+TheRealKymac@users.noreply.github.com>
2017-08-15 19:29:47 -0700

    Update timeAndDateDisplay_client.lua

--- timeAndDateDisplay_client.lua
+++ timeAndDateDisplay_client.lua
@@ -3,99 +3,116 @@ local useMilitaryTime = false
 local displayDayOfWeek = true
 local displayDate = true
 
--- Display Time and Date at top right of screen
+local timeAndDateString = nil
+local hour = 1
+local minute = 0
+local dayOfWeek = 1
+local month = 1
+local dayOfMonth = 1
+local year = 2017
+
+-- Display Time and Date at top right of screen -- format: | 12:13 | Wednesday | January 17, 2017 |
 Citizen.CreateThread(function()
-	
-	while true do -- format: | 12:13 | Wednesday | January 17, 2017 |
-		Citizen.Wait(1)
-		local timeAndDateString = "|"
+	while true do
+		Wait(1)
+		timeAndDateString = "|"
 		
-		if displayTime then
-			local hour = GetClockHours()
-			local minute = GetClockMinutes()
-		
-			if !useMilitaryTime then
-				if hour == 0 or hour == 24 then
-					hour = 12
-				elseif hour >= 13 then
-					hour = hour - 12
-				end
-			end
-
-			if hour <= 9 then
-				hour = "0" .. hour
-			end
-			
-			if minute <= 9 then
-				minute = "0" .. minute
-			end
-
+		if displayTime == true then
+			CalculateTimeToDisplay()
 			timeAndDateString = timeAndDateString .. " " .. hour .. ":" .. minute .. " |"
 		end
-
-		if displayDayOfWeek then
-			local dayOfWeek = GetClockDayOfWeek()
-			if dayOfWeek == 0 then
-				dayOfWeek = "Sunday"
-			elseif dayOfWeek == 1 then
-				dayOfWeek = "Monday"
-			elseif dayOfWeek == 2 then
-				dayOfWeek = "Tuesday"
-			elseif dayOfWeek == 3 then
-				dayOfWeek = "Wednesday"
-			elseif dayOfWeek == 4 then
-				dayOfWeek = "Thursday"
-			elseif dayOfWeek == 5 then
-				dayOfWeek = "Friday"
-			elseif dayOfWeek == 6 then
-				dayOfWeek = "Saturday"
-			end
+		if displayDayOfWeek == true then
+			CalculateDayOfWeekToDisplay()
 			timeAndDateString = timeAndDateString .. " " .. dayOfWeek .. " |"
 		end
-
-		if displayDate then
-			local month = GetClockMonth()
-			local dayOfMonth = GetClockDayOfMonth()
-			local year = GetClockYear()
-			
-			if month == 0 then
-				month = "January"
-			elseif month == 1 then
-				month = "February"
-			elseif month == 2 then
-				month = "March"
-			elseif month == 3 then
-				month = "April"
-			elseif month == 4 then
-				month = "May"
-			elseif month == 5 then
-				month = "June"
-			elseif month == 6 then
-				month = "July"
-			elseif month == 7 then
-				month = "August"
-			elseif month == 8 then
-				month = "September"
-			elseif month == 9 then
-				month = "October"
-			elseif month == 10 then
-				month = "November"
-			elseif month == 11 then
-				month = "December"
-			end
+		if displayDate == true then
+			CalculateDateToDisplay()
 			timeAndDateString = timeAndDateString .. " " .. month .. " " .. dayOfMonth .. ", " .. year .. " |"
 		end
+		
 		SetTextFont(0)
 		SetTextProportional(1)
-		SetTextScale(0.0, 0.55)
+		SetTextScale(0.30, 0.30)
 		SetTextColour(255, 255, 255, 255)
 		SetTextDropshadow(0, 0, 0, 0, 255)
 		SetTextEdge(1, 0, 0, 0, 255)
 		SetTextDropShadow()
 		SetTextOutline()
 		SetTextEntry("STRING")
-
+		
 		AddTextComponentString(timeAndDateString)
-		DrawText(0.985, 0.5)
+		DrawText(0.8, 0.01)
 	end
 end)
+
+function CalculateTimeToDisplay()
+	hour = GetClockHours()
+	minute = GetClockMinutes()
+
+	if useMilitaryTime == false then
+		if hour == 0 or hour == 24 then
+			hour = 12
+		elseif hour >= 13 then
+			hour = hour - 12
+		end
+	end
+
+	if hour <= 9 then
+		hour = "0" .. hour
+	end
+	if minute <= 9 then
+		minute = "0" .. minute
+	end
+end
+
+function CalculateDayOfWeekToDisplay()
+	dayOfWeek = GetClockDayOfWeek()
+	
+	if dayOfWeek == 0 then
+		dayOfWeek = "Sunday"
+	elseif dayOfWeek == 1 then
+		dayOfWeek = "Monday"
+	elseif dayOfWeek == 2 then
+		dayOfWeek = "Tuesday"
+	elseif dayOfWeek == 3 then
+		dayOfWeek = "Wednesday"
+	elseif dayOfWeek == 4 then
+		dayOfWeek = "Thursday"
+	elseif dayOfWeek == 5 then
+		dayOfWeek = "Friday"
+	elseif dayOfWeek == 6 then
+		dayOfWeek = "Saturday"
+	end
+end
+
+function CalculateDateToDisplay()
+	month = GetClockMonth()
+	dayOfMonth = GetClockDayOfMonth()
+	year = GetClockYear()
+	
+	if month == 0 then
+		month = "January"
+	elseif month == 1 then
+		month = "February"
+	elseif month == 2 then
+		month = "March"
+	elseif month == 3 then
+		month = "April"
+	elseif month == 4 then
+		month = "May"
+	elseif month == 5 then
+		month = "June"
+	elseif month == 6 then
+		month = "July"
+	elseif month == 7 then
+		month = "August"
+	elseif month == 8 then
+		month = "September"
+	elseif month == 9 then
+		month = "October"
+	elseif month == 10 then
+		month = "November"
+	elseif month == 11 then
+		month = "December"
+	end
+end
